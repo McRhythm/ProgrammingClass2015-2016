05609f2c5011dfc842b4d5bbdb8ee767cb08999b
McRhythm <mchiu@chicagotechacademy.org>
2015-09-18 10:18:24 -0500

    Create assignment4.rb

---
+++ assignment4.rb
@@ -0,0 +1,20 @@
+how much hours do you work?
+hours=gets.to_i
+radius=hours/2
+puts”the radius is#{radius}
+=>40
+
+how much do you make per hour?
+$=gets.to_i
+radius=$/2
+puts”the radius is#{radius}
+=>20
+
+how many weeks did you work?
+weeksr=gets.to_i
+radius=weeks/2
+puts”the radius is#{radius}
+=>10
+
+you will make $8000.
+=>nil
